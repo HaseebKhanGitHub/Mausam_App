4bee0e7321a1c07ba9bafd0cfab4318d9470834a
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-08-08 14:53:30 +0500

    Create worker.dart

---
+++ lib/Worker/worker.dart
@@ -0,0 +1,64 @@
+import 'package:http/http.dart' as http;
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:mausam_app/main.dart';
+
+class worker {
+  String location;
+
+  worker({required this.location}) {
+    location = this.location;
+  }
+
+  late String temp;
+  late String humidity;
+  late String air_speed;
+  late String description;
+  late String main;
+
+  Future<void> getData() async {
+
+    try{
+      var url = Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?q=$location&appid=9f24f45f1a43cd906106cb78433a7be0');
+      http.Response response =
+      await http.get(url); // Use the Uri object with http.get
+      Map data = jsonDecode(response.body); // Decode the JSON response
+
+      //temperature, humidity
+      Map temp_data = data['main'];
+      double getTemp = temp_data['temp'];
+      String getHumidity = temp_data['humidity'].toString();
+
+      //Getting AirSpeed
+      Map wind = data['wind'];
+      double getAirspeed = wind['speed'];
+
+      //Getting Description
+      List weather_data = data['weather']; //list type
+      Map weather_main_data = weather_data[0]; //dictionary type
+      String getMaindes = weather_main_data['main'];
+      String getDesc = weather_main_data['description'];
+
+      //Assigning values
+      temp = getTemp.toString();
+      humidity = getHumidity;
+      air_speed = getAirspeed.toString();
+      description = getDesc;
+      main = getMaindes;
+    } catch(e)
+    {
+      temp = 'Cannot Find Data';
+      humidity = 'Cannot Find Data';
+      air_speed = 'Cannot Find Data';
+      description = 'Cannot Find Data';
+      main = 'Cannot Find Data';
+    }
+    }
+
+    }
+
+
+
